9c0146624f8d7a5d562845fa08f11b455f360323
Simranjit2 <simranjit1090@gmail.com>
2022-02-26 18:32:00 -0500

    Updating Executive and Intern class

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -4,7 +4,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern 
+public class Intern extends StaffEmployee
 {
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
@@ -12,7 +12,7 @@ public class Intern
     //-----------------------------------------------------------------
     public Intern(String eName, String eAddress, String ePhone)
     {
-        
+        super(eName, eAddress, ePhone);
     }
 
     //-----------------------------------------------------------------
@@ -20,6 +20,6 @@ public class Intern
     //-----------------------------------------------------------------
     public double pay()
     {
-      
+      return 0;
     }
 }
