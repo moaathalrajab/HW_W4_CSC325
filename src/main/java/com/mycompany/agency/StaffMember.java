d90969c20b7566ea6e6bcaf125e99ff25740e01c
jonathan-crain <124228802+jonathan-crain@users.noreply.github.com>
2023-02-15 15:40:56 -0500

    Update StaffMember.java
    Implemented toString() method.

--- src/main/java/com/mycompany/agency/StaffMember.java
+++ src/main/java/com/mycompany/agency/StaffMember.java
@@ -1,7 +1,5 @@
 package com.mycompany.agency;
 
-
-
 // TODO 1: Make this class work and public --DONE--
 
  public abstract class StaffMember
@@ -22,11 +20,11 @@ package com.mycompany.agency;
     }
 
     //-----------------------------------------------------------------
-    // TODO 2: Returns a string including the basic employee information.
+    // TODO 2: Returns a string including the basic employee information. --DONE--
     //-----------------------------------------------------------------
     public String toString()
     {
-
+        return String.format("Name: %s Address: %s Phone: %s ", name, address, phone);
     }
 
     //-----------------------------------------------------------------
