291751e24caff35f46d77188a9f4eab4d6b52f23
NRShah25 <103145108+NRShah25@users.noreply.github.com>
2023-02-15 14:51:09 -0500

    Made intern child of staff employee class

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -4,7 +4,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern 
+public class Intern extends StaffEmployee
 {
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
