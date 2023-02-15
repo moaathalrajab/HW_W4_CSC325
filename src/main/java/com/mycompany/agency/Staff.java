17924776a9e2ed8de9b3295b8d339145d1f83622
Steven Miladinović <95955605+sdmila@users.noreply.github.com>
2023-02-15 16:44:08 -0500

    Update Staff.java

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -11,11 +11,17 @@ public class Staff {
         staffList = new StaffMember[6];
 
         // TODO 1: Add 6 emploees to the staffList --DONE
+        staffList[0] = new Executive("Jayson", "CSC325", "000-000-0001", "000-00-0001", 1.00);
         ((Executive) staffList[0]).awardBonus(500.00);
+        staffList[1] = new Intern("Nick", "CSC325", "000-000-0000");
         ((Intern) staffList[1]).pay();
+        staffList[2] = new StaffEmployee("Crain", "CSC325", "000-000-0002", "000-00-0002", 2.00);
         ((StaffEmployee) staffList[2]).pay();
+        staffList[3] = new TempEmploee("Vasquez", "CSC325", "000-000-0003", "000-00-0003", 1.25);
         ((TempEmploee) staffList[3]).addHours(40);
+        staffList[4] = new StaffEmployee("Steve", "CSC325", "000-000-0004", "000-00-0004", 9999.99);
         ((StaffEmployee) staffList[4]).pay();
+        staffList[5] = new StaffEmployee("Mazen", "CSC325", "000-000-0005", "000-00-0005", 0.50);
         ((StaffEmployee) staffList[5]).pay();
     }
 
