a983bc09a4fefeb640ba54cf5c5fab54771cdf51
NRShah25 <103145108+NRShah25@users.noreply.github.com>
2023-02-15 15:26:39 -0500

    Staff Employee extends Staff Member

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -3,7 +3,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make StaffEmployee a child of StaffMember
 
-public class StaffEmployee 
+public class StaffEmployee extends StaffMember
 {
     protected String socialSecurityNumber;
     protected double payRate;
